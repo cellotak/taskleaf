a78bb9e8ff912588fa128e849b2fe855a0792eb2
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-17 19:01:18 +0900

    ログインフォームの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
   namespace :admin do
     resources :users
   end
-  
+
   root to: 'tasks#index'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
